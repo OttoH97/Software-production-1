28c2fe0054905297253b1681fdaeee09d7840583
Pinja Hämäläinen <Pinja.Hamalainen1@gmail.com>
2022-02-23 14:16:28 +0200

    Poista- ja Muokkaa-napit lisätty Matkakohdesivulle

--- src/matkakohde_sivu.js
+++ src/matkakohde_sivu.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
@@ -10,67 +10,88 @@ function Matkakohdesivu() {
 
     const data = [
         {
-          "id": 5,
-          "nimi": "Lontoo",
+            "id": 5,
+            "kohdenimi": "Pariisi",
+            "maa": "Ranska",
+            "kuvausteksti": "Kaupunki"
         },
         {
-            "id": 5,
-            "nimi": "Berliini",
-          },
+            "id": 6,
+            "kohdenimi": "Berliini",
+            "maa": "Saksa",
+            "kuvausteksti": "Kaupunki"
+        },
         {
-            "id": 8,
-            "nimi": "Helsinki"
+            "id": 7,
+            "kohdenimi": "Helsinki",
+            "maa": "Suomi",
+            "kuvausteksti": "Kaupunki"
         }
-        ]
+    ]
 
     const [nimi, setNimi] = useState('');
     const [matkakohteet, setMatkakohteet] = useState(data);
 
-    return(
-<div>
-    <Navbar bg="light" expand="lg">
-        <Container>
-        <Navbar.Brand href="#home">Matkakertomus</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Koti</Nav.Link>
-                <Nav.Link href="#link">Tiedot</Nav.Link>
-                <Nav.Link href="#link">Matkat</Nav.Link>
-            <NavDropdown title="Droppi" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">tänne jotain</NavDropdown.Item>          
-            </NavDropdown>
-            </Nav>
-        </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Matkakertomus</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Koti</Nav.Link>
+                            <Nav.Link href="#link">Tiedot</Nav.Link>
+                            <Nav.Link href="#link">Matkat</Nav.Link>
+                            <NavDropdown title="Droppi" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">tänne jotain</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-    <h3 style={{backgroundColor: "lightgray"}}>Matkakohteet</h3><br></br>
-    <label>
-        Hae matkakohteita: <br></br>
-        <input type="text" value={nimi} onChange={(e) =>setNimi(e.target.value)}/>
-    </label>
-    <button>Hae</button><br></br>
+            <h3 style={{ backgroundColor: "lightgray" }}>Matkakohteet</h3><br></br>
+            <label>
+                Hae matkakohteita: <br></br>
+                <input type="text" value={nimi} onChange={(e) => setNimi(e.target.value)} />
+            </label>
+            <button>Hae</button><br></br>
 
-    <table className="taulu">
-    <thead>
-            <tr>
-                <th scope="col">
-                Matkakohde
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        {matkakohteet.map((matkakohde) => (
-            <tr key={matkakohde.id}>
-                <td>{matkakohde.nimi}</td>
-            </tr>
-           ))}
-            </tbody>
-        </table>
+            <table className='taulu'>
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            Matkakohde
+                        </th>
+                        <th scope="col">
+                            Maa
+                        </th>
+                        <th scope="col">
+                            Kuvaus
+                        </th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {matkakohteet.map((matkakohde) => (
+                        <tr key={matkakohde.id}>
+                            <td>
+                                <img src='https://i.pravatar.cc/100' alt="" />
+                                {matkakohde.kohdenimi}
+                            </td>
+                            <td>{matkakohde.maa}</td>
+                            <td>{matkakohde.kuvausteksti}</td>
+                            <td><button>Poista</button></td>
+                            <td><button>Muokkaa</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
-    </div>
+        </div>
     )
-    
+
 }
-export {Matkakohdesivu};
+export { Matkakohdesivu };
